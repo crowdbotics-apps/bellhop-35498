0e7a61fb9daba720bfcd2fc626b07479c932e53b
Crowdbotics <team@crowdbotics.com>
2022-05-29 21:01:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/bellhop_35498/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bellhop_35498;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "bellhop_35498";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "bellhop_35498",
+  url: "https://bellhop_35498.botics.co",
+  api: "https://bellhop_35498.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
